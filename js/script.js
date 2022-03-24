ce6cb713cb7fd33cada11f20e93da359d637057b
unknown <gerard.lopez.garcia@gmail.com>
2022-03-24 22:48:02 +0100

    some fixes in Chrome Extension

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let  sections = document.getElementById("clickable")
 let  out = document.getElementById("body")
 
 sections.addEventListener("click",function changeMe(){
-    sections.innerHTML = `<iframe src="chromeE\\index.html" frameborder="0" width="100%"height="500em"></iframe>`
+    sections.innerHTML = `<iframe src="chromeE\\index.html" frameborder="0" width="100%"height="190px"></iframe>`
     // setInterval(function () {
     //     sections.innerHTML = `<img  src="img/extension.png" alt="porject-image" >`
     // },7000)    
