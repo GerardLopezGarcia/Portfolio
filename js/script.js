5819dbadb62c2c64828cda54df766d6c3bedb38a
unknown <gerard.lopez.garcia@gmail.com>
2022-03-30 21:53:53 +0200

    2nd page

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
+var elements = document.getElementById('imagenes');
+var positionInfos = elements.getBoundingClientRect();
+var heights = positionInfos.height;
+var widths = positionInfos.width;
+console.log(heights,widths)
 let  sections = document.getElementById("clickable")
 
 
-sections.addEventListener("click",function changeMe(){
-    sections.innerHTML = `<iframe src="chromeE\\index.html" frameborder="0" width="100%"height="190px"></iframe>`
+sections.addEventListener("click", function changeMe (){
+    sections.innerHTML =`<iframe src="chromeE\\index.html" frameborder="0" width="${heights}"height="${heights}"></iframe>`
     // setInterval(function () {
     //     sections.innerHTML = `<img  src="img/extension.png" alt="porject-image" >`
-    // },7000)    
-      
-});
+    // },7000) 
+})
 
 addEventListener("scroll",function () {
     sections.innerHTML = `<img  src="img/extension.png" alt="porject-image" >`
     console.log("sfgsdfgsdf")
 })
 
+// let api = document.getElementById("ClickMe")
+// console.log(api)
+// api.addEventListener("click",function (){
+//     api.innerHTML = `<iframe src="Weather_Api\\index_weatherAPI.html" frameborder="0"></iframe>`
+//     console.log("api")
+// })
+//INTENT DE FERHO RE-USABLE ---------------------------------------------------------------
+// function changeMe(object,url) {
+//     object.innerHTML = `<iframe src=${url}></iframe>`
+//     console.log(object,url)
+// }
